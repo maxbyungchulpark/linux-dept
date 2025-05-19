b34e799a3777fbb79dee58ab4afb2b44649a89a8
Byungchul Park <byungchul@sk.com>
2025-05-19 18:18:13 +0900

    cpu/hotplug: use a weaker annotation in AP thread
    cb92173d1f0 ("locking/lockdep, cpu/hotplug: Annotate AP thread") was
    introduced to make lockdep_assert_cpus_held() work in AP thread.
    
    However, the annotation is too strong for that purpose.  We don't have
    to use more than try lock annotation for that.
    
    rwsem_acquire() implies:
    
       1. might be a waiter on contention of the lock.
       2. enter to the critical section of the lock.
    
    All we need in here is to act 2, not 1.  So trylock version of
    annotation is sufficient for that purpose.  Now that dept partially
    relies on lockdep annotaions, dept interpets rwsem_acquire() as a
    potential wait and might report a deadlock by the wait.
    
    Replace it with trylock version of annotation.
    
    Signed-off-by: Byungchul Park <byungchul@sk.com>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -537,7 +537,7 @@ int lockdep_is_cpus_held(void)
 
 static void lockdep_acquire_cpus_lock(void)
 {
-	rwsem_acquire(&cpu_hotplug_lock.dep_map, 0, 0, _THIS_IP_);
+	rwsem_acquire(&cpu_hotplug_lock.dep_map, 0, 1, _THIS_IP_);
 }
 
 static void lockdep_release_cpus_lock(void)
