23f21a76e362eb9564a78b1a9f90c44f53390d43
Byungchul Park <byungchul@sk.com>
2026-08-25 23:11:10 +0900

    dept: track timeout waits separately with a new Kconfig
    Waits with valid timeouts don't actually cause deadlocks.  However, dept
    has been reporting the cases as well because it's worth informing the
    circular dependency for some cases where, for example, timeout is used
    to avoid a deadlock.
    
    However, yes, there are also a lot of, even more, cases where timeout
    is used for its clear purpose and meant to be expired.
    
    Report these as an information rather than warning DEADLOCK.  Plus,
    introduce CONFIG_DEPT_AGGRESSIVE_TIMEOUT_WAIT Kconfig to make it
    optional so that any reports involving waits with timeouts can be turned
    on/off depending on the purpose.
    
    Signed-off-by: Byungchul Park <byungchul@sk.com>

--- include/linux/dept.h
+++ include/linux/dept.h
@@ -139,8 +139,8 @@ void dept_free_range(void *start, unsigned int sz);
 void dept_map_init(struct dept_map *m, struct dept_key *k, int sub_u, const char *n);
 void dept_map_reinit(struct dept_map *m, struct dept_key *k, int sub_u, const char *n);
 void dept_map_copy(struct dept_map *to, struct dept_map *from);
-void dept_wait(struct dept_map *m, unsigned long w_f, unsigned long ip, const char *w_fn, int sub_l);
-void dept_stage_wait(struct dept_map *m, struct dept_key *k, unsigned long ip, const char *w_fn);
+void dept_wait(struct dept_map *m, unsigned long w_f, unsigned long ip, const char *w_fn, int sub_l, long timeout);
+void dept_stage_wait(struct dept_map *m, struct dept_key *k, unsigned long ip, const char *w_fn, long timeout);
 void dept_request_event_wait_commit(void);
 void dept_clean_stage(void);
 void dept_ttwu_stage_wait(struct task_struct *t, unsigned long ip);
@@ -187,8 +187,8 @@ struct dept_map { };
 #define dept_map_init(m, k, su, n)			do { (void)(n); (void)(k); } while (0)
 #define dept_map_reinit(m, k, su, n)			do { (void)(n); (void)(k); } while (0)
 #define dept_map_copy(t, f)				do { } while (0)
-#define dept_wait(m, w_f, ip, w_fn, sl)			do { (void)(w_fn); } while (0)
-#define dept_stage_wait(m, k, ip, w_fn)			do { (void)(k); (void)(w_fn); } while (0)
+#define dept_wait(m, w_f, ip, w_fn, sl, t)		do { (void)(w_fn); } while (0)
+#define dept_stage_wait(m, k, ip, w_fn, t)		do { (void)(k); (void)(w_fn); } while (0)
 #define dept_request_event_wait_commit()		do { } while (0)
 #define dept_clean_stage()				do { } while (0)
 #define dept_ttwu_stage_wait(t, ip)			do { } while (0)
